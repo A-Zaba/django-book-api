8dbe2e7857257d25af2e6c42c0a627e3fd1eda72
A-Zaba <Zabala.e.andres@gmail.com>
2020-12-15 20:27:29 -0500

    Added Travis-CI and Flake8 configs. Updated requirements.txt. Set up Docker.

--- app/books/models.py
+++ app/books/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from datetime import datetime
+# from django.utils.translation import ugettextlazy as _
+
 
 # Create your models here.
+class User(AbstractBaseUser, PermissionsMixin):
+    pass
+
